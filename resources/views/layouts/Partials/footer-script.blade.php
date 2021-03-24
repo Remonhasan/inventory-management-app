2612ea75bb93fa2502e569a32610e0fb19fb4ca3
remonhasan <remonhasan041@gmail.com>
2021-03-24 15:35:31 +0600

    added partials for admin dashboard

---
+++ resources/views/layouts/Partials/footer-script.blade.php
@@ -0,0 +1,21 @@
+<script src="{{asset('admin/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('admin/dist/js/adminlte.min.js')}}"></script>
+<script src="{{ asset('admin/plugins/datatables/jquery.dataTables.js') }}"></script>
+<script src="{{ asset('admin/plugins/datatables-bs4/js/dataTables.bootstrap4.js') }}"></script>
+<script>
+    $(".datatable").DataTable();
+    $('.datatable').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+    });
+</script>
+<x:notify-messages />
+@notifyJs
+
